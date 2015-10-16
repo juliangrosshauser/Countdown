55c731d540fe8e6c84469416f2314ef351ed7cd2
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-17 00:02:37 +0200

    Add Xcode project

---
+++ CountdownUITests/Source/CountdownUITests.swift
@@ -0,0 +1,18 @@
+//
+//  CountdownUITests.swift
+//  CountdownUITests
+//
+//  Created by Julian Grosshauser on 16/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+
+class CountdownUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+}
