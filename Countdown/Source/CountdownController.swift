927238c12f879fdded52e8bfca833e5afb5061a2
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-19 22:17:43 +0200

    Add `CountdownController` view controller

---
+++ Countdown/Source/CountdownController.swift
@@ -0,0 +1,11 @@
+//
+//  CountdownController.swift
+//  Countdown
+//
+//  Created by Julian Grosshauser on 19/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class CountdownController: UIViewController {}
