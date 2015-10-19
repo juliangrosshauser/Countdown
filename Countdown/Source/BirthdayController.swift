889d52e15420345b8c3d07d97348ea0e9bc41e42
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-19 22:35:48 +0200

    Add `BirthdayController` view controller

---
+++ Countdown/Source/BirthdayController.swift
@@ -0,0 +1,11 @@
+//
+//  BirthdayController.swift
+//  Countdown
+//
+//  Created by Julian Grosshauser on 19/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import UIKit
+
+class BirthdayController: UIViewController {}
