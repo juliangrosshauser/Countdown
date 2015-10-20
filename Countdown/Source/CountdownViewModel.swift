43e2ea09149225c2c953e9186a9c5ae4f712b6f2
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-20 23:10:13 +0200

    Add `CountdownViewModel` view model

---
+++ Countdown/Source/CountdownViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  CountdownViewModel.swift
+//  Countdown
+//
+//  Created by Julian Grosshauser on 20/10/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+class CountdownViewModel {}
