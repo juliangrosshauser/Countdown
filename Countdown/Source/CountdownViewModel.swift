1d832f434ca4783b73ab1edf5cd9a0162b3c16fd
Julian Grosshauser <julian@juliangrosshauser.com>
2015-10-30 23:11:33 +0100

    Add `ageForBirthday(_:)` method

--- Countdown/Source/CountdownViewModel.swift
+++ Countdown/Source/CountdownViewModel.swift
@@ -31,4 +31,27 @@ class CountdownViewModel {
     init(userDefaults: NSUserDefaults) {
         self.userDefaults = userDefaults
     }
+
+    //MARK: Age Calculation
+
+    // Credits: https://github.com/soffes/Motivation/blob/master/Motivation/AgeView.swift
+    private func ageForBirthday(birthday: NSDate) -> Double {
+        let calendar = NSCalendar.currentCalendar()
+        let now = NSDate()
+        let ageComponents = calendar.components([.Year, .Day, .Second, .Nanosecond], fromDate: birthday, toDate: now, options: [])
+
+        let daysInYear = Double(calendar.daysInYear(now) ?? 365)
+        let hoursInDay = Double(calendar.rangeOfUnit(.Hour, inUnit: .Day, forDate: now).length)
+        let minutesInHour = Double(calendar.rangeOfUnit(.Minute, inUnit: .Hour, forDate: now).length)
+        let secondsInMinute = Double(calendar.rangeOfUnit(.Second, inUnit: .Minute, forDate: now).length)
+        let nanosecondsInSecond = Double(calendar.rangeOfUnit(.Nanosecond, inUnit: .Second, forDate: now).length)
+
+        let seconds = Double(ageComponents.second) + Double(ageComponents.nanosecond) / nanosecondsInSecond
+        let minutes = seconds / secondsInMinute
+        let hours = minutes / minutesInHour
+        let days = Double(ageComponents.day) + hours / hoursInDay
+        let years = Double(ageComponents.year) + days / daysInYear
+        
+        return years
+    }
 }
