7d02fd01567db8be0e550df2659c9e3d47d62cda
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-03 00:46:50 +0100

    Test `daysInYear(_:)`

--- CountdownKitTests/Source/NSCalendarCountdownTests.swift
+++ CountdownKitTests/Source/NSCalendarCountdownTests.swift
@@ -9,4 +9,29 @@
 import XCTest
 @testable import CountdownKit
 
-class NSCalendarCountdownTests: XCTestCase {}
+class NSCalendarCountdownTests: XCTestCase {
+
+    func testDaysInYear() {
+        let calendar = NSCalendar.currentCalendar()
+        let dateComponents = NSDateComponents()
+
+        // Test every year from 1700 to 2200
+        for year in 1700...2200 {
+            dateComponents.year = year
+            guard let date = calendar.dateFromComponents(dateComponents) else {
+                XCTFail("Can't create date object from components")
+                return
+            }
+            
+            let days = calendar.daysInYear(date)
+
+            // Every year that's divisible by 4, but not 100,
+            // and every year that's divisible by 400 is a leap year
+            if (year % 4 == 0 && year % 100 != 0) || year % 400 == 0 {
+                XCTAssertEqual(days, 366)
+            } else {
+                XCTAssertEqual(days, 365)
+            }
+        }
+    }
+}
