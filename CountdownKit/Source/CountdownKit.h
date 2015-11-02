de27685b279ebd433fa680fb30428d8ddd1bb292
Julian Grosshauser <julian@juliangrosshauser.com>
2015-11-02 23:33:40 +0100

    Create `CountdownKit` framework plus tests

---
+++ CountdownKit/Source/CountdownKit.h
@@ -0,0 +1,15 @@
+//
+//  CountdownKit.h
+//  CountdownKit
+//
+//  Created by Julian Grosshauser on 02/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CountdownKit.
+FOUNDATION_EXPORT double CountdownKitVersionNumber;
+
+//! Project version string for CountdownKit.
+FOUNDATION_EXPORT const unsigned char CountdownKitVersionString[];
---
+++ CountdownKitTests/Source/CountdownKitTests.swift
@@ -0,0 +1,12 @@
+//
+//  CountdownKitTests.swift
+//  CountdownKitTests
+//
+//  Created by Julian Grosshauser on 02/11/15.
+//  Copyright © 2015 Julian Grosshauser. All rights reserved.
+//
+
+import XCTest
+@testable import CountdownKit
+
+class CountdownKitTests: XCTestCase {}
